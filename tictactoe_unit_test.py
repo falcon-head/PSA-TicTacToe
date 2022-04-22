4d558067cecbd33c8de2713763de219ca1f3c813
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2022-04-22 18:59:09 -0400

    Add the condition model 2

--- tictactoe_unit_test.py
+++ tictactoe_unit_test.py
@@ -2,7 +2,7 @@ import unittest
 import tictactoe_reinforcement
 import numpy as np
 import pandas as pd
-from mock import Mock
+from unittest.mock import Mock
 
 # Only one test case possible in tictactoe_reinforcement.py
 class TestTicTacToe(unittest.TestCase):
@@ -19,6 +19,7 @@ class TestTicTacToe(unittest.TestCase):
         self.assertEqual(tictactoe_reinforcement.PlayerTraining.get_latest_board_values(self, np.array([[0, 0, 0], [0, 0, 0], [1, 0, 0]])), str(np.array([[0, 0, 0], [0, 0, 0], [1, 0, 0]]).reshape(3*3)))
         self.assertEqual(tictactoe_reinforcement.PlayerTraining.get_latest_board_values(self, np.array([[0, 0, 0], [0, 0, 0], [0, 1, 0]])), str(np.array([[0, 0, 0], [0, 0, 0], [0, 1, 0]]).reshape(3*3)))
         self.assertEqual(tictactoe_reinforcement.PlayerTraining.get_latest_board_values(self, np.array([[0, 0, 0], [0, 0, 0], [0, 0, 1]])), str(np.array([[0, 0, 0], [0, 0, 0], [0, 0, 1]]).reshape(3*3)))
+
 
 if __name__ == "__main__":
     unittest.main()
